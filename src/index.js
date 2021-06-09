c61ed74f5b963216e00a62d105a4689842401511
Anatoliy Mostovoy <mostovoyanatoliy@gmail.com>
2021-06-09 06:40:45 +0300

    Сделал обновление страницы
    1. При выборе нового запроса происходит очистка галлереи

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import { refs } from './js/variables.js';
 import cardData from './templates/card';
 import NewsApiService from './js/apiService';
 
-console.log(refs.loadBtn);
-
 refs.searchForm.addEventListener('submit', onSearch);
 refs.moreBtn.addEventListener('click', onLoadMore);
 
@@ -16,18 +14,22 @@ function onSearch(event) {
   newsApiService.query = event.currentTarget.elements.query.value.trim();
   newsApiService.resetPage();
   newsApiService.fetchArticles().then(renderCard);
+  refs.gallery.innerHTML = '';
 }
 
 function onLoadMore() {
   newsApiService.fetchArticles().then(renderCard);
-
-  refs.loadBtn.scrollIntoView({
-    behavior: 'smooth',
-    block: 'end',
-  });
+  scrollImg();
 }
 
 function renderCard(data) {
   const doCard = cardData(data);
   refs.gallery.insertAdjacentHTML('beforeend', doCard);
+}
+
+function scrollImg() {
+  refs.moreBtn.scrollIntoView({
+    behavior: 'smooth',
+    block: 'end',
+  });
 }
--- src/js/variables.js
+++ src/js/variables.js
@@ -2,5 +2,4 @@ export const refs = {
   searchForm: document.querySelector('.search-form'),
   gallery: document.querySelector('.gallery'),
   moreBtn: document.querySelector('.button-more'),
-  loadBtn: document.getElementById('my-element-selector'),
 };
